86a941792b4c88c7c8655496f4c4c70db3dc94b3
V-Yashasvi <vyashasvi1@gmail.com>
2024-10-01 11:51:41 +0530

    hello

--- app.js
+++ app.js
@@ -7,12 +7,23 @@ function moreAboutHome(address,distanceFromTown,hasNeighbours){
 }
 //Trial 2:
 //Check if the data given is of the right type
-//parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
-
+// parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
+function moreAboutKaren(parents,noOfSiblings,isNuclearFamily){
+    return (typeof(parents)+typeof(noOfSiblings)+typeof(isNuclearFamily))
+}
 //Trial 3:
 //Lily is suspicious about Karen's new friend
 //Karen tells her friend's age and even writes it down
 //Check which one those is not a number (NaN) and return that value
+function newfriend(ageInText,ageInNumber){
+if (isNaN(ageInText)){
+    return ageInText;
+}else if (isNan(ageInNumber)){
+    return ageInNumber;
+}else{
+    return null;
+}
+}
 
 //Trial 4:
 //Lily gave Karen x sweets
@@ -20,10 +31,18 @@ function moreAboutHome(address,distanceFromTown,hasNeighbours){
 //On her way to the river, she ate another z sweets every n meters travelled
 //Her friend divided the remaining sweets into 2 parts for each
 //How many sweets did her friend get to eat?
+function sweetTooth(totalNoOfSweets,sweetsConsumedByKaren,sweetsConsumedInNMeters,metersToTravel){
+totalNoOfSweets-=sweetsConsumedByKaren+(sweetsConsumedInNMeters*metersToTravel)
+totalNoOfSweets/=2;
+return totalNoOfSweets
+}
 
 //Trial 5:
 //As Lily moves closer, it gets colder. She checks the temperature on her mobile
 //It only shows in fahrenheit. Convert the data to celsius and return it.
+function convertToCelcius(farenheight){
+    return(farenheight-32)*5/9;
+}
 
 //Trial 6:
 //Lily can now do multiple things to deal with this
@@ -32,6 +51,19 @@ function moreAboutHome(address,distanceFromTown,hasNeighbours){
 //3. Counsel her daughter herself
 //4. Lock her daughter in her room
 //Given a value, return which of these above actions Lily would take
+function aDifficultChoice(){
+    if (choice==1){
+        return("Take her to a doctor")
+    }else if(choice==2){
+        return("Talk to her husband about it")
+    }else if(choice==3){
+        return("Counsel her daughter herself")
+    }else if(choice==4){
+        return("Look her daughter in her room")
+    }}
+
+    
+
 
 //Challenge 1:
 //Lily realized that she'd hurt her daughter
@@ -41,3 +73,7 @@ function moreAboutHome(address,distanceFromTown,hasNeighbours){
 //Take all of Lily's strategies and concatenate them to a single var
 //Seperate the strategies by a single space
 //Return the length of the complete strategy
+function consoleKaren(stratergies){
+    let arr=stratergies.join(' ');
+    return arr.length;
+}
